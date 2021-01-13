d6fff5d993aab0c5c38720c3178415b4003d10a5
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-01-13 13:32:16 +0900

    Update discordbot_v2.2.5.py

--- discordbot_v2.2.5.py
+++ discordbot_v2.2.5.py
@@ -79,7 +79,8 @@ class Calc(commands.Cog):
     async def ent(self, ctx, p: str):
         """Entropy P()"""
         p = eval(p);
-        await ctx.send(-p*math.log2(p)-(1-p)*math.log2(p)*math.log2(1-p))
+        await ctx.send(-p*math.log2(p)-(1-p)*math.log2(1-p)*math.log2(1-p))
+
 
 #---------------------------------------------------------- B系
 class B(commands.Cog):
