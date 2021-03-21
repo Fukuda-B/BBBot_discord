b375d4d74fdd44fe4a175af2c14b6ef10d2c6dd5
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-03-21 19:59:00 +0900

    v2.3.3
    Separated api keys.
    Fixed EqCheck Class.

---
+++ my_key.py
@@ -0,0 +1,15 @@
+# API keys are separated from v2.3.3
+def get_keys():
+    TOKEN=''
+
+    A3RT_URI='https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
+    A3RT_KEY=''
+
+    GoogleTranslateAPP_URL=''
+
+    LOG_C=  # LOGS Channel
+    # MAIN_C= # SEND Channel (本番用)
+    MAIN_C= # SEND Channel (試験用)
+    VOICE_C= # VOICE Channel (試験用)
+
+    return TOKEN, A3RT_URI, A3RT_KEY, GoogleTranslateAPP_URL, LOG_C, MAIN_C, VOICE_C
