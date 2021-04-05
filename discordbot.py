71d342d594e6aed72a845ee2c6364ed254f6c3f1
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-04-05 16:36:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -563,7 +563,7 @@ class Timer(commands.Cog):
                 await asyncio.sleep(5)
                 await dest.edit(content = f'{mention}'+" | "+str(setCnt_ - setCnt + 1)+"/"+str(setCnt_)+" break left: "+ str(int(time2/60)).zfill(2)+":"+str(int(time2%60)).zfill(2))
             setCnt -= 1
-        await dest.edit(content = f'{mention}'+" Good jobbb!\nRecord: cnt="+str(setCnt)+", work: "+str(int(time_/60))+"(min) / break: "+str(int(time2_/60))+"(min)")
+        await dest.edit(content = f'{mention}'+" Good jobbb!\nRecord: cnt="+str(setCnt_)+", work: "+str(int(time_/60))+"(min) / break: "+str(int(time2_/60))+"(min)")
 
 
 # Botの起動とDiscordサーバーへの接続
